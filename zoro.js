05dbbed74f458c12a1c7eafbb1060f355072e086
Praveenrajhhgg <praveenraj04012002@gamil.com>
2024-02-24 12:00:42 +0530

    first commit

---
+++ zoro.js
@@ -0,0 +1,11 @@
+// var sidenav = document.querySelector('.side')
+// sidenav.style.display= "block"
+// console.log("sidenav")
+
+// var navbarmenu = document.querySelector(".navbar-menu-toggle")
+// var side = document.querySelector(".side")
+// navbarmenu.addEventListener("click",function(){
+// side.style.display="block"
+// })
+var side = document.querySelector(".side")
+side.style.display = "block"
